f4a4bf97c138472806a32993fb3659c7897e86e1
Blaise Skoletsky <blaiseskoletsky@gmail.com>
2024-01-18 22:08:09 -0800

    Modified

---
+++ blaise-react/src/components/IndivProject.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Aboutme from './buttons/AboutMeButton'
+import Contact from './buttons/ContactButton'
+import Project from './buttons/ProjectButton'
+
+function IndivProject() {
+
+
+
+    return (
+  
+          
+            <div className='flex flex-row justify-center h-48'>
+              <h1 className = 'text-3xl basis-2/3 justify-center text-center'>Render Description Here</h1>
+              <img alt='image failed to load'></img>
+            </div>
+         
+  
+    );
+  }
+
+export default IndivProject
--- blaise-react/src/pages/Projects.js
+++ blaise-react/src/pages/Projects.js
@@ -1,14 +1,20 @@
 import React from 'react'
+import IndivProject from '../components/IndivProject';
+
+
 function Project(){
 
     return (
-        <div className='h-32'>
-            <h1 className='text-cyan-400  font-mono text-8xl text-center'> Portfolio</h1>
+        <div>
+            <div className='h-32'>
+                <h1 className='text-cyan-400  font-mono text-7xl text-center'> Portfolio</h1>
+            </div>
+            <div>
+                <IndivProject />
+                <IndivProject />
+            </div>
         </div>
-
     );
 }
-
-
 
 export default Project
