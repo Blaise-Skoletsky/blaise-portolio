5f81d3210ae4b271a7e7298945d149bc8d6ff6ca
Blaise Skoletsky <blaiseskoletsky@gmail.com>
2024-02-07 17:57:45 -0800

    Added some text.

--- blaise-react/src/pages/About.js
+++ blaise-react/src/pages/About.js
@@ -5,8 +5,15 @@ import React from 'react'
 function About (){
 
     return (
-
-       <p>About Page</p>
+    <div> 
+        <p className="text-white font-mono text-7xl text-center">Welcome!</p>
+        <div className="text-slate-100 font-mono text-xl m-20">
+            <p className="my-10">Hey there! I'm Blaise, a dynamic software engineering enthusiast with a burning passion for crafting innovative solutions. Hailing from Portland Oregon, and currently pursuing my studies at Oregon State University, I'm fueled by curiosity and a relentless drive to push the boundaries of what's possible in the digital realm.</p>
+            <p className="my-10">Outside the realm of coding, you'll often find me immersed in various side projects, experimenting with emerging technologies, or simply enjoying a good cup of coffee while brainstorming the next big idea.</p>
+            <p className="my-10">Let's connect and embark on this exciting journey together!</p>
+        </div> 
+    </div>
+       
 
     );
 }
--- blaise-react/src/pages/Projects.js
+++ blaise-react/src/pages/Projects.js
@@ -8,9 +8,9 @@ function Project(){
     return (
         <div>
             <div className='h-32'>
-                <h1 className='text-cyan-400  font-mono text-7xl text-center'> Portfolio</h1>
+                <h1 className='text-slate-100  font-mono text-7xl text-center'> Portfolio</h1>
             </div>
-            <div className='justify-center'>
+            <div className='justify-center text-slate-100' >
                 <IndivProject url={pokerImg} description={'Multiplayer Poker game between 4-8 players!'} tools={'FULLSTACK - [Node.js - JS- HTML - CSS - Socket.io]'}/>
                 <IndivProject url={pokerImg} description={'Linear regression to determine housing prices.'} tools={'ML - [Python - Pytorch]'}/>
             </div>
